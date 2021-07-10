9e13f2468c93af070f8d49720b100f3134f44e56
Marcus Vinícius Ribeiro do Nascimento <marcusvrdon@gmail.com>
2021-07-10 20:45:49 -0300

    refactor: adjust all styles imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,10 @@
-import '../styles/globals.scss'
+import '@/styles/globals.scss'
+import '@/styles/home.scss'
+import '@/styles/game.scss'
+import '@/styles/card.scss'
+import '@/styles/door.scss'
+import '@/styles/gift.scss'
+import '@/styles/input-number.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
