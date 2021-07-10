3b66b55587b441f42cd7989fc3d301af2409b132
Marcus Vinícius Ribeiro do Nascimento <marcusvrdon@gmail.com>
2021-07-10 20:42:30 -0300

    feat: add Gift component

---
+++ components/Gift/index.tsx
@@ -0,0 +1,12 @@
+function Gift() {
+  return (
+    <div className="gift">
+      <div className="gift__cover"></div>
+      <div className="gift__body"></div>
+      <div className="gift__ribbon gift__ribbon--horizontal"></div>
+      <div className="gift__ribbon gift__ribbon--vertical"></div>
+    </div>
+  )
+}
+
+export default Gift
